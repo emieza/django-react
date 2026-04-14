d9f5d2b774962c0e4067f43aba80a229b85a70af
enric <emieza@xtec.cat>
2026-04-14 18:31:25 +0200

    CSRF_TRUSTED_ORIGINS a settings

--- djangoreact/settings.py
+++ djangoreact/settings.py
@@ -136,11 +136,6 @@ CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS",default=[
     "http://127.0.0.1:5173",
 ])
 
-"""CORS_ALLOW_CREDENTIALS = True
+# protecció CSRF per atacs de dominis creuats
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS")
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]"""
-
-print(CORS_ALLOWED_ORIGINS)
